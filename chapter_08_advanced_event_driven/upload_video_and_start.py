c1d3387e621930e3c9399588abf18c59265d9027
Jaime Buelta <jaime.buelta@gmail.com>
2021-12-29 13:48:22 +0100

    Add empty credentials

--- chapter_08_advanced_event_driven/upload_video_and_start.py
+++ chapter_08_advanced_event_driven/upload_video_and_start.py
@@ -14,7 +14,9 @@ SOURCE_VIDEO_PATH = '/source_video.mp4'
 @click.command()
 @click.argument('video_to_upload')
 def main(video_to_upload):
-    client = boto3.client('s3', endpoint_url=MOCK_S3)
+    client = boto3.client('s3', endpoint_url=MOCK_S3,
+                          aws_access_key_id='ACCESS_ID',
+                          aws_secret_access_key='ACCESS_KEY')
     # Create bucket if not set
     client.create_bucket(Bucket=BUCKET)
 
